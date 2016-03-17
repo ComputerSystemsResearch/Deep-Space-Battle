a048dcf146b4beeb0987e96bd2ad39bbdaa143fc
HarrisonLloyd <harrylloyd45@gmail.com>
2016-03-17 14:39:26 -0400

    Gameplay Update

--- GAME/src/state/gameplay/MatchConstants.java
+++ GAME/src/state/gameplay/MatchConstants.java
@@ -8,9 +8,9 @@
    	
 		public static final int PAUSE_KEY_CODE = KeyEvent.VK_ESCAPE;
       public static final int[] P1_KEY_CODES = new int[] { KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_A, KeyEvent.VK_D,
-                                        KeyEvent.VK_I, KeyEvent.VK_O, KeyEvent.VK_P };
+                                                           KeyEvent.VK_I, KeyEvent.VK_O, KeyEvent.VK_P };
       public static final int[] P2_KEY_CODES = new int[] { KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT,
-                                        KeyEvent.VK_NUMPAD1, KeyEvent.VK_NUMPAD2, KeyEvent.VK_NUMPAD3 };
+                                                           KeyEvent.VK_NUMPAD1, KeyEvent.VK_NUMPAD2, KeyEvent.VK_NUMPAD3 };
    	
       public static final int TICKS_PER_SECOND = 25;
       public static final int SKIP_TICKS = 1000000000 / TICKS_PER_SECOND;
